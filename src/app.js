b4021f1ae07ff3f3d7dbc45253f19ecd9a06681e
racer242 <racer242@gmail.com>
2022-05-05 17:00:45 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import createError from "http-errors";
+import logger from "morgan";
+
+import path from "path";
+
+import TelegramBot from "./telegramBot/TelegramBot";
+
+const telegramBot = new TelegramBot();
+
+telegramBot.init();
+
+telegramBot.start();
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(telegramBot.webhookCallback());
+
+app.get("/", async (req, res) => {
+  res.json({ status: true, message: "Our node.js app works" });
+});
+
+// catch 404 and forward to error   handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler //, next
+app.use(function (err, req, res) {
+  res.status(err.status || 500);
+  res.end(err.message);
+});
+
+export default app;
---
+++ src/configuration/locales.js
@@ -0,0 +1,27 @@
+const locales = {
+  en: {
+    isDefault: true,
+    start: `👋 Hello! I'm a bot 🤖!
+I serve people 👨‍👩‍👦‍👦.
+Write something ✍️.
+I will translate it into another language 📓
+    `,
+    help: `How can I help you man?
+    `,
+    commands: {
+      help: "I'll help in any way I can",
+    },
+  },
+  ru: {
+    start: `👋 Привет! Я бот 🤖!
+Я служу человекам 👨‍👩‍👦‍👦.
+`,
+    help: `Чем тебе помочь, человек❓
+    `,
+    commands: {
+      help: "Помогу, чем смогу",
+    },
+  },
+};
+
+export default locales;
